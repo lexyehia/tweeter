54a5323c99c16cab1d5ccaf13c5e2060ad5b544d
Lex Yehia <lex.yehia@icloud.com>
2017-10-17 00:15:46 -0400

    Tweet submission validations

--- public/scripts/compose-tweet-form.js
+++ public/scripts/compose-tweet-form.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
     $('.new-tweet').find('form').submit(function (e) {
-        console.log($(this).serialize());
-        $.post( "/tweets/", $(this).serialize());
-        return false;
+        var input = $(this).find('textarea').val();
+
+        if (input === null || input === '') {
+            alert('Please enter some text first!');
+        } else if (input.length > 140) {
+            alert('Your tweet is way too long!');
+        } else {
+            $.post( "/tweets/", $(this).serialize());
+        }
+
+        return false;        
     })
 })
