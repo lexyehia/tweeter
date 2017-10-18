af35d8c7d520241169883f075101dfe7c1be53ec
Lex Yehia <lex.yehia@icloud.com>
2017-10-18 15:24:04 -0400

    First commit for refactoring

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const morgan        = require('morgan')
 const path          = require('path')
 const cookieParser  = require('cookie-parser')
 
+// Set middle-ware
 app.use(morgan('dev'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
@@ -18,28 +19,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 app.use(cookieParser())
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
+// TODO: Remove DataHelpers - useless with mongoose
 const DataHelpers = require("./lib/data-helpers.js")();
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 const usersRoutes = require('./routes/users');
 
-// Mount the tweets routes at the "/tweets" path prefix:
-
+// API Routers
 app.use("/tweets", tweetsRoutes)
 app.use("/users", usersRoutes)
 
+// Start server
 app.listen(PORT, () => {
     console.log("Example app listening on port " + PORT);
 });
