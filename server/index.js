33011be4592a6c6545dec317c805249a04b7453f
Lex Yehia <lex.yehia@icloud.com>
2017-10-18 13:35:27 -0400

    Minor fixes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 require('dotenv').config({path: __dirname + '/.env'});
 
 // Basic express setup:
-const PORT          = 8080
+const PORT          = process.env.PORT || 8080
 const express       = require('express')
 const bodyParser    = require('body-parser')
 const app           = express()
